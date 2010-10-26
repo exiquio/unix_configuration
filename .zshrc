1fb0626cc213c1408704d768ac890d42158193ff
Exiquio Cooper-Anderson <exiquio@gmail.com>
2010-10-25 23:16:47 -0600

    change symlinks to hardlinks. hope that works:)

--- .zshrc
+++ .zshrc
@@ -1,1 +1,41 @@
-/home/exiquio/.zshrc
+# Lines configured by zsh-newuser-install
+HISTFILE=~/.histfile
+HISTSIZE=1000
+SAVEHIST=1000
+setopt appendhistory extendedglob nomatch notify
+unsetopt autocd beep
+bindkey -v
+# End of lines configured by zsh-newuser-install
+# The following lines were added by compinstall
+zstyle :compinstall filename '/home/ghost/.zshrc'
+
+autoload -Uz compinit
+compinit
+# End of lines added by compinstall
+
+# User Added
+autoload -U promptinit
+promptinit
+prompt elite white green 
+
+# Exports
+export GREP_COLOR="1;33"
+export LESS_TERMCAP_mb=$'\e[1;31m'
+export LESS_TERMCAP_md=$'\e[1;31m'
+export LESS_TERMCAP_me=$'\e[0m'
+export LESS_TERMCAP_se=$'\e[0m'
+export LESS_TERMCAP_so=$'\e[1;44;33m'
+export LESS_TERMCAP_ue=$'\e[0m'
+export LESS_TERMCAP_us=$'\e[1;32m'
+export PATH="$PATH:/home/ghost/Scripts:/opt/android-sdk/tools"
+export IRCNICK="exiquio"
+export IRCSERVER="irc.freenode.net"
+export EDITOR="vim"
+
+# Aliases
+alias cp='cp -v'
+alias grep='grep -i --color=auto'
+alias ls='ls --color=auto'
+
+# Evals
+eval `dircolors -b`
--- xmonad.hs
+++ xmonad.hs
@@ -1,1 +1,84 @@
-/home/exiquio/.xmonad/xmonad.hs
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Tabbed
+import XMonad.Util.Run
+
+import System.IO
+
+import qualified Data.Map as M
+
+main = do
+	xmproc <- spawnPipe "xmobar ~/.xmobarrc"
+	xmonad $ defaultConfig
+		{
+			terminal = "urxvt",
+			workspaces = ["web", "office", "media", "code", "misc", "admin"],
+			normalBorderColor = "#000000",
+			focusedBorderColor = "#FFFFFF",
+			manageHook =
+				composeOne
+					[
+						isFullscreen -?> doFullFloat,
+						isDialog -?> doCenterFloat
+					]
+				<+> composeAll
+					[
+						className =? "Chromium" --> doShift "web",
+						className =? "Amarok" --> doShift "media",
+						className =? "MPlayer" --> doShift "media",
+						className =? "Smplayer" --> doShift "media",
+						className =? "Okular" --> doShift "misc",
+						className =? "Search" --> doFloat, 
+						className =? "MPlayer" --> doFloat,
+						className =? "Thunar" --> doFloat,
+						className =? "Gimp" --> doFloat,
+						className =? "Feh" --> doFloat,
+						className =? "Pyvolwheel" --> doFloat,
+						className =? "Alsamixer-qt4" --> doFloat,
+						className =? "Adeskmenu" --> doFloat,
+						className =? "Glista" --> doFloat
+					]
+				<+> manageDocks,
+			layoutHook = myLayout,
+			logHook = dynamicLogWithPP $ xmobarPP
+				{
+					ppOutput = hPutStrLn xmproc,
+					ppTitle = xmobarColor "green" "" . shorten 50
+				},
+			keys = \c -> myKeys c `M.union` keys defaultConfig c
+		}
+		where
+			nmaster = 1
+			ratio = 1/2
+			delta = 3/100
+			tiled = Tall nmaster delta ratio
+			myTabConfig = defaultTheme {
+				inactiveBorderColor = "#000000",
+				inactiveTextColor = "#000000",
+				inactiveColor = "#FFFFFF",
+				activeBorderColor = "#FFFFFF",
+				activeTextColor = "#FFFFFF",
+				activeColor = "#000000"
+			}
+			myLayout = avoidStruts $ smartBorders $ tiled
+				||| Mirror tiled
+				||| Full
+				||| tabbed shrinkText myTabConfig
+			myKeys (XConfig {modMask = modm}) = M.fromList $
+				[
+					((modm, xK_t), spawn "glista"),
+					((modm, xK_s), spawn "beagle-search"),
+					((modm, xK_f), spawn "thunar"),
+					((modm, xK_p), spawn "dmenu_run -fn  'xft:-schumacher-clean-bold-r-normal--10-129-75-75-c-80-*-*' -nb '#000000' -nf '#FFFFFF' -sb '#000000' -sf '#FFFFFF' -i"),
+					((modm, xK_l), spawn "hslock"),
+					((modm, xK_F1), spawn "eject"),
+					((modm, xK_F3), spawn "aumix -v -3"),
+					((modm, xK_F6), spawn "~/Scripts/brightness.rb up"),
+					((shiftMask, xK_F6), spawn "~/Scripts/brightness.rb down"),
+					((modm, xK_F7), spawn "aumix -v +3"),
+					((modm, xK_F8), spawn "amixer set Master toggle"),
+					((0, xK_Print), spawn "scrot -e 'mv $f ~/Images/Screenshots'")
+				]
