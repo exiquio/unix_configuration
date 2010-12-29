bbc78bc5cd625d09a5918dd64b0c1ad94e789118
Exiquio Cooper-Andrson <exiquio@gmail.com>
2010-12-29 09:19:57 -0700

    Revert "#"
    This reverts commit c6eb76a1e9b57063b394929724328088cc6cb7ce.

--- .zshrc
+++ .zshrc
@@ -1,1 +1,65 @@
-/home/exiquio/.zshrc
+# Lines configured by zsh-newuser-install
+HISTFILE=~/.histfile
+HISTSIZE=10000
+SAVEHIST=10000
+setopt appendhistory extendedglob nomatch notify
+unsetopt autocd beep
+bindkey -v
+# End of lines configured by zsh-newuser-install
+# The following lines were added by compinstall
+zstyle :compinstall filename '${HOME}/.zshrc'
+
+autoload -Uz compinit
+compinit
+# End of lines added by compinstall
+
+
+###########################################################
+# User Added #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+autoload -U promptinit
+promptinit
+prompt elite green white
+
+# Exports
+export GREP_COLOR="1;33"
+export LESS_TERMCAP_mb=$'\e[1;31m'
+export LESS_TERMCAP_md=$'\e[1;31m'
+export LESS_TERMCAP_me=$'\e[0m'
+export LESS_TERMCAP_se=$'\e[0m'
+export LESS_TERMCAP_so=$'\e[1;44;33m'
+export LESS_TERMCAP_ue=$'\e[0m'
+export LESS_TERMCAP_us=$'\e[1;32m'
+export IRCNICK="exiquio"
+export IRCSERVER="irc.freenode.net"
+export EDITOR="vim"
+export BROWSER="chromium"
+export JDK_HOME=${JAVA_HOME}
+export JRE_HOME=${JAVA_HOME}
+export JRUBY_HOME="/opt/local/jruby"
+export CATALINA_HOME="/srv/tomcat"
+export CODE_HOME="${HOME}/Code"
+export DWT_REPO="${CODE_HOME}/DWT"
+export C2LIB="${DWT_REPO}/c2_lib_trunk/lib"
+export C2ENV="DEV"
+export C2_CONNECTOR_HOME="${DWT_REPO}/c2_connector_trunk"
+export C2_CONNECTORS="${C2_CONNECTOR_HOME}/connector"
+export STRATA_HOME="${C2_CONNECTORS}/ruby"
+export C2_TEST_BASEDIR="${STRATA_HOME}"
+export CHARLES_HOME="/opt/local/charles"
+export ADK_HOME="/opt/android-sdk"
+export ANT_HOME="/usr/share/java/apache-ant"
+export PERSONAL_CODE_HOME="${CODE_HOME}/Personal"
+export CELTX_HOME="/opt/celtx"
+export REPOS="${DWT_REPO}/dwt:${C2LIB}:${C2_CONNECTOR_HOME}:${PERSONAL_CODE_HOME}/Linux-Configuration-Files:${PERSONAL_CODE_HOME}/LinuxScripts:${PERSONAL_CODE_HOME}/BodyBuilder"
+export PATH="${PATH}:${HOME}/bin:$CELTX_HOME:$CHARLES_HOME/bin:${ADK_HOME}/tools:${ADK_HOME}/platform-tools:${JRUBY_HOME}/bin:${JAVA_HOME}/bin:${ANT_HOME}/bin:${CATALINA_HOME}/bin"
+
+# Aliases
+alias cp='cp -v'
+alias grep='grep -i --color=auto'
+alias ls='ls --color=auto'
+alias open='xdg-open'
+
+# Evals
+eval `dircolors -b`
--- xmonad.hs
+++ xmonad.hs
@@ -1,1 +1,89 @@
-/home/exiquio/.xmonad/xmonad.hs
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Tabbed
+import XMonad.Util.Run
+
+import System.IO
+
+import qualified Data.Map as M
+
+main = do
+	xmproc <- spawnPipe "xmobar ~/.xmobarrc"
+	xmonad $ defaultConfig
+		{
+			terminal = "urxvt",
+			workspaces = ["web", "office", "media", "code", "misc", "admin"],
+			normalBorderColor = "#000000",
+			focusedBorderColor = "#FFFFFF",
+			manageHook =
+				composeOne
+					[
+						isFullscreen -?> doFullFloat,
+						isDialog -?> doCenterFloat
+					]
+				<+> composeAll
+					[
+						className =? "Chromium" --> doShift "web",
+						className =? "Amarok" --> doShift "media",
+						className =? "Mplayer" --> doShift "media",
+						className =? "Mplayer" --> doFloat,
+						className =? "Okular" --> doShift "misc",
+						className =? "Skype" --> doShift "office",
+						className =? "Skype" --> doFloat,
+						className =? "Catfish.pyc" --> doFloat,
+						className =? "Gimp" --> doShift "misc",
+						className =? "Gimp" --> doFloat,
+						className =? "Alsamixer-qt4" --> doShift "media",
+						className =? "Alsamixer-qt4" --> doFloat,
+						className =? "Visual-regexp" --> doShift "misc",
+						className =? "Visual-regexp" --> doFloat,
+						className =? "Adeskmenu" --> doFloat,
+						className =? "Pyvolwheel" --> doFloat,
+						className =? "Thunar" --> doFloat,
+						className =? "Gui_test.rb" --> doShift "misc",
+						className =? "Gui_test.rb" --> doFloat,
+						className =? "Krunner" --> doIgnore
+					]
+				<+> manageDocks,
+			layoutHook = myLayout,
+			logHook = dynamicLogWithPP $ xmobarPP
+				{
+					ppOutput = hPutStrLn xmproc,
+					ppTitle = xmobarColor "green" "" . shorten 50
+				},
+			keys = \c -> myKeys c `M.union` keys defaultConfig c
+		}
+		where
+			nmaster = 1
+			ratio = 1/2
+			delta = 3/100
+			tiled = Tall nmaster delta ratio
+			myTabConfig = defaultTheme {
+				inactiveBorderColor = "#000000",
+				inactiveTextColor = "#000000",
+				inactiveColor = "#FFFFFF",
+				activeBorderColor = "#FFFFFF",
+				activeTextColor = "#FFFFFF",
+				activeColor = "#000000"
+			}
+			myLayout = avoidStruts $ smartBorders $ tiled
+				||| Mirror tiled
+				||| Full
+				||| tabbed shrinkText myTabConfig
+			myKeys (XConfig {modMask = modm}) = M.fromList $
+				[
+					((modm, xK_s), spawn "catfish"),
+					((modm, xK_f), spawn "thunar"),
+					((modm, xK_p), spawn "dmenu_run -fn  'xft:Dejavu Sans Mono-7:antialiased=true' -nb '#000000' -nf '#FFFFFF' -sb '#000000' -sf '#FFFFFF' -i"),
+					((modm, xK_l), spawn "hslock"),
+					((modm, xK_F1), spawn "eject"),
+					((modm, xK_F3), spawn "aumix -v -2"),
+					((modm, xK_F6), spawn "~/bin/brightness.rb up"),
+					((shiftMask, xK_F6), spawn "~/bin/brightness.rb down"),
+					((modm, xK_F7), spawn "aumix -v +2"),
+					((modm, xK_F8), spawn "amixer set Master toggle"),
+					((0, xK_Print), spawn "scrot -e 'mv $f ~/Pictures/Screenshots'")
+				]
